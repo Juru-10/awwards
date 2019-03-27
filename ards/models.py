50c0d784ff6a0a154b05e639dc2ea89424367eb8
JURU <jurassu10@gmail.com>
2019-03-27 16:59:28 +0200

    add the Project class in app models.py file

--- ards/models.py
+++ ards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=30)
+    image=models.ImageField(upload_to = 'ards/',default='SOME STRING')
+    description=models.CharField(max_length=300)
+    link=models.CharField(max_length=100)
