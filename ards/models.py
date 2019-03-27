49a5d427d871f4a744f557d31b756782617306e3
JURU <jurassu10@gmail.com>
2019-03-27 17:05:20 +0200

    add the user Profile class in app models.py file

--- ards/models.py
+++ ards/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Project(models.Model):
-    title=models.CharField(max_length=30)
-    image=models.ImageField(upload_to = 'ards/',default='SOME STRING')
-    description=models.CharField(max_length=300)
-    link=models.CharField(max_length=100)
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'ards/',default='SOME STRING')
+    description = models.CharField(max_length=300)
+    link = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    prof_pic = models.ImageField(upload_to = 'ards/',default='SOME STRING')
+    bio = models.CharField(max_length=300)
+    posts = models.ForeignKey(Projects)
+    email = models.CharField(max_length=30)
