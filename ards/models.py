2bea96f6e62ef1525ab862497da6b76ba4d70d11
JURU <jurassu10@gmail.com>
2019-03-29 12:14:50 +0200

    add the reviews field in the project model

--- ards/models.py
+++ ards/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+class Review(models.Model):
+    design = models.RatingField(range=10)
+    usability = models.RatingField(range=10)
+    content = models.RatingField(range=10)
+
 class Project(models.Model):
     title = models.CharField(max_length=30)
     image = models.ImageField(upload_to = 'ards/',default='SOME STRING')
     description = models.CharField(max_length=300)
     link = models.CharField(max_length=100)
+    reviews = models.ForeignKey(Review)
 
 class Profile(models.Model):
     prof_pic = models.ImageField(upload_to = 'ards/',default='SOME STRING')
     bio = models.CharField(max_length=300)
     posts = models.ForeignKey(Project)
     contact = models.CharField(max_length=30)
-
-class Review(models.Model):
-    design = models.RatingField(range=10)
-    usability = models.RatingField(range=10)
-    content = models.RatingField(range=10)
