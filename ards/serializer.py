a00828851e80932a7c7daeea6ae535b68302c951
JURU <jurassu10@gmail.com>
2019-03-29 11:50:42 +0200

    add the ids to the serializer classes to be able to get single item form the list

--- ards/serializer.py
+++ ards/serializer.py
@@ -4,9 +4,9 @@ from .models import Profile,Project
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = (prof_pic,bio,posts,contact)
+        fields = (id,prof_pic,bio,posts,contact)
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = (title,image,description,link)
+        fields = (id,title,image,description,link)
