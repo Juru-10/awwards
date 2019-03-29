a5850cff127962a32c555d96bc5c072c87790519
JURU <jurassu10@gmail.com>
2019-03-29 11:26:28 +0200

    add the API lists classes in views.py file

--- ards/views.py
+++ ards/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Profile,Project
+from .serializer import ProjectSerializer,ProfileSerializer
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
